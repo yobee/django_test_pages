f2f2c7463bc591bad28ea10da684fef446ceda7f
yobe <smail21yoshi@gmail.com>
2019-10-12 14:11:54 +0900

    New updates for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web:gincorn pages_project.wsgi --log-file-
--- pages_project/settings.py
+++ pages_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#x$6ug%*%3q49-9__&vjuijoknw8v#z%yc@za@dji79=l(7d5@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
